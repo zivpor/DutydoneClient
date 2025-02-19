9149b77cfb73ea801c585d8ba8bbbb2dd3db9b47
User <User@LAB3-3>
2025-02-19 15:29:59 +0200

    jfeiush9f

--- Services/DutyDoneAPIProxy.cs
+++ Services/DutyDoneAPIProxy.cs
@@ -328,5 +328,37 @@ namespace DutydoneClient.Services
                 return null;
             }
         }
+        public async Task<List<Models.Task>?> GetTasks()
+        {
+            //Set URI to the specific function API
+            string url = $"{this.baseUrl}GetTasks";
+            try
+            {
+                //Call the server API
+                HttpResponseMessage response = await client.GetAsync(url);
+                //Check status
+                if (response.IsSuccessStatusCode)
+                {
+                    //Extract the content as string
+                    string resContent = await response.Content.ReadAsStringAsync();
+                    //Desrialize result
+                    JsonSerializerOptions options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                    List<Models.Task>? result = JsonSerializer.Deserialize<List<Models.Task>>(resContent, options);
+                    return result;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
+
 }
--- ViewModels/TasksListViewModel.cs
+++ ViewModels/TasksListViewModel.cs
@@ -1,9 +1,50 @@
+using DutydoneClient.Models;
+using DutydoneClient.Services;
+using System.Collections.ObjectModel;
+
 namespace DutydoneClient.ViewModels;
 
 public class TasksListViewModel : ViewModelBase
 {
-	public TasksListViewModel()
+	public Command FilterCommand { get; set; }
+	private DutyDoneAPIProxy proxy;
+	private List<Models.Task> allTasks;
+	private ObservableCollection<Models.Task> filterdTasks;
+    public ObservableCollection<Models.Task> FilterdTasks
 	{
-		
+		get
+		{
+			return this.filterdTasks;
+		}
+		set
+		{
+			this.filterdTasks = value;
+			OnPropertyChanged();
+		}
+	}
+    public TasksListViewModel(DutyDoneAPIProxy proxy)
+	{
+		this.proxy = proxy;
+		this.FilterCommand = new Command(Filter);
+		allTasks = new List<Models.Task>();
+		FilterdTasks = new ObservableCollection<Models.Task>();
+		filtered = false;
+		ReadTasks();
+	}
+	private async void ReadTasks()
+	{
+		allTasks = await proxy.GetTasks();
+		if (allTasks == null)
+			allTasks = new List<Models.Task>();
+	}
+	private bool filtered;
+	public async void Filter()
+	{
+		filtered = true;
+		FilterdTasks.Clear();
+		foreach(Models.Task t in this.allTasks)
+		{
+			if()
+		}
 	}
 }
