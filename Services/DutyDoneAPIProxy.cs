4112c6d0cc4078363dea55af190bb475fa927701
User <User@LAB3-3>
2025-05-07 13:10:26 +0300

    gkjrhgolb

--- Services/DutyDoneAPIProxy.cs
+++ Services/DutyDoneAPIProxy.cs
@@ -485,6 +485,33 @@ namespace DutydoneClient.Services
                 return false;
             }
         }
+        public async Task<bool> UpdateUser(User user)
+        {
+            //Set URI to the specific function API
+            string url = $"{this.baseUrl}update";
+            try
+            {
+                //Call the server API
+                string json = JsonSerializer.Serialize(user);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                //Check status
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
 
     }
 
--- Views/EditProfile.xaml.cs
+++ Views/EditProfile.xaml.cs
@@ -1,9 +1,12 @@
+using DutydoneClient.ViewModels;
+
 namespace DutydoneClient.Views;
 
 public partial class EditProfile : ContentPage
 {
-	public EditProfile()
+	public EditProfile(EditProfileViewModel vm)
 	{
+		this.BindingContext= vm;
 		InitializeComponent();
 	}
 }
